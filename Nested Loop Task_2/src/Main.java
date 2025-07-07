161245b6b866653eeb49d98056d111332c6a0a36
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-07 04:34:36 -0700

    added Task

--- Nested Loop Task_2/src/Main.java
+++ Nested Loop Task_2/src/Main.java
@@ -8,12 +8,17 @@ public class Main {
         // x ^ 0 + x ^ 1 + x ^ 3 + x ^ 4 + x ^ 5 .....
 
         int x = 2;
-        for (int i = 1; i <= 5; i++) {
+        int n = 3;
+        int summa = 0;
+        for (int i = 0; i <= n; i++) {
+            int pow =1;
             for (int j = 1; j <= i; j++) {
-                System.out.print(i);
+                pow *= x;
             }
-            System.out.println();
+            //System.out.println(pow);
+            summa += pow;
         }
+        System.out.println("summa = " + summa);
 
     }
 }
