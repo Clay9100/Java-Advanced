378c5e89de5948bd6112dae24a0f8b3abdaca08b
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-07 08:52:54 -0700

    added

--- Geometric Shape/src/Main.java
+++ Geometric Shape/src/Main.java
@@ -116,9 +116,7 @@ public class Main {
                 System.out.print(" ");
             }
             for (int j = 0; j <= i; j++) {
-
                 System.out.print("* ");
-
             }
             System.out.println();
         }
