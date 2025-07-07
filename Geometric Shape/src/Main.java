1ce0f4056448aa4de90c9c5af093b97aff116bc3
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-07 08:06:14 -0700

    added new shape

--- Geometric Shape/src/Main.java
+++ Geometric Shape/src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         // this is lesson geometric shape
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter row: ");
+
+        int row = in.nextInt();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+
 
 
     }
