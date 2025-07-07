ba9182caad0993dc1bea16434cec496d2828c6f7
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-07 07:13:56 -0700

    added for in continue

--- Continue in Nested loop/src/Main.java
+++ Continue in Nested loop/src/Main.java
@@ -5,6 +5,24 @@ public class Main {
 
         // this is lesson continue in nested loop
 
+        /*for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(j == 2){
+                    continue;
+                }
+                System.out.println(i + " " + j);
+            }
+        }*/
+
+        for (int i = 0; i < 5; i++) {
+            if(i == 1){
+                continue;
+            }
+            for (int j = 0; j < 5; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+
 
 
     }
