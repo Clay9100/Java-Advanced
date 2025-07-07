71690afb4a8c951af4b3f880648f13e089d676f4
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-07 04:58:00 -0700

    added while loop write logical

--- Polindrom Number Task_1/src/Main.java
+++ Polindrom Number Task_1/src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         // this is Task Polindrom number
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int x = input.nextInt();
+
+        int sum = 0;
+        while (x != 0) {
+            int q = x % 10;
+            sum = sum * 10 + q;
+            //System.out.println(q);
+            x /= 10;
+        }
+
+        System.out.println(sum);
+
 
 
     }
