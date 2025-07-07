8969a7e70e166ed0d50b0792bab05662f1802f38
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-07 02:53:24 -0700

    added for sikl operator Table

--- Karra Table/src/Main.java
+++ Karra Table/src/Main.java
@@ -6,6 +6,13 @@ public class Main {
 
         // for
 
+        for (int i = 2; i < 10 ; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, (i * j));
+            }
+            System.out.println();
+        }
+
 
     }
 }
