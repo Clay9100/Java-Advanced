684a651071e5289f7b8cd1d2a2de170236cfacaa
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-08 11:06:51 -0700

    added find element

--- Array In Find Element/src/Main.java
+++ Array In Find Element/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +12,25 @@ public class Main {
         for (int i : k) {
             System.out.printf("%d, ", i);
         }
+
+        System.out.print("\nEnter find element: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        boolean check = false;
+        for (int i : k) {
+            if (i == n) {
+                check = true;
+                break;
+            }
+        }
+        if (check) {
+            System.out.printf("This is %d element find", n);
+        } else {
+            System.out.printf("This is %d not find element ", n);
+        }
+
+
 
     }
 }
