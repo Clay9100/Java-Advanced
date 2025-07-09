37a83a049cf4a7a8a5d6f29681d4366b9a15041e
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-09 08:22:20 -0700

    added array double create

--- Array Create/src/Main.java
+++ Array Create/src/Main.java
@@ -5,13 +5,23 @@ public class Main {
 
         // this is lesson array create
 
-        int[] k = {1, 2, 3, 4, 5};
+       /* int[] k = {1, 2, 3, 4, 5};
         int[] v = new int[]{1, 2, 3, 4, 5};
-        int[] f = {};
+        int[] f = {};*/
 
         // System.out.println(k[0]);
         // System.out.println(v[0]);
         // System.out.println(f[0]); // error zero element not create
+
+        // double
+
+        double[] d = new double[]{9.8, 4, 5, 6, 2.3};
+        double[] d1 = new double[]{};
+        double[] d2 = {};
+
+        System.out.println(d[0]);
+        System.out.println(d1[0]);
+        System.out.println(d2[0]);
 
     }
 }
