a5c3f45e4d9f94577050f85ecbdce7e6d47945a0
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-09 08:57:08 -0700

    added new lesson Polindrom Number create break and continue

---
+++ Polindrom Number_2/src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Polindrom number in create break
+
+
+
+         // break
+
+        /*for (int i = 2; i < 100; i++) {
+            boolean tubmi = true;
+            for (int j = 2; j < i ; j++) {
+                if(i % j == 0) {
+                    tubmi = false;
+                    break;
+                }
+            }
+            if (tubmi) {
+                System.out.println(i);
+            }
+        }*/
+
+    }
+}
