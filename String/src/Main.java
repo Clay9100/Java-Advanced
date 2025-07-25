a3a1244786326c951486157cfee865df82d0b23b
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-25 15:20:09 +0500

    added declare String variable s1 s2

--- String/src/Main.java
+++ String/src/Main.java
@@ -1,9 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
         // this is lesson String data type
+
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        String s3 = new String("Hello");
+
+        System.out.println(s1 == s2); // true
+
+        System.out.println(s1 == s3); // false
+
 
 
     }
