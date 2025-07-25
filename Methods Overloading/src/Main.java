98a7d7d4e82e95762efae2706f044fdf8ca1b353
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-25 14:22:11 +0500

    added more params input value

--- Methods Overloading/src/Main.java
+++ Methods Overloading/src/Main.java
@@ -16,9 +16,34 @@ public class Main {
        /* byte max = maxNumber((byte) 4,(byte) 5);
         System.out.println("maxN = " + max);*/
 
-        double max = maxNumber(4.3,5);
+        /*double max = maxNumber(4.3,5);
+        System.out.println("maxN = " + max);*/
+
+       /* int max = maxNumber(4,5,3);
+        System.out.println("maxN = " + max);*/
+
+        double max = maxNumber(4,5, 8,4, 34, 12,-4);
         System.out.println("maxN = " + max);
 
+
+
+    }
+
+    public static double maxNumber(double ...n ){
+        double max = n[0];
+
+        for(double i : n){
+            if(max < i){
+                max = i;
+            }
+        }
+        return max;
+
+
+    }
+    public static int maxNumber(int x, int y, int z) {
+        int k = maxNumber(x, y);
+        return k > z? k : z;
     }
 
     public static double maxNumber(double x, int y){
