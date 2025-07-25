a083e4dd4446828b5101723f1e69846d85fff90f
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-25 14:13:46 +0500

    added new lesson Methods overloading

---
+++ Methods Overloading/src/Main.java
@@ -0,0 +1,43 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // this is methods overloading
+
+        /*int max = maxNumber(4,5);
+        System.out.println("maxN = " + max);*/
+
+        /*double max = maxNumber(4.7,5.4);
+        System.out.println("maxN = " + max);*/
+
+        /*float max = maxNumber(4.3f,5.4f);
+        System.out.println("maxN = " + max);*/
+
+       /* byte max = maxNumber((byte) 4,(byte) 5);
+        System.out.println("maxN = " + max);*/
+
+        double max = maxNumber(4.3,5);
+        System.out.println("maxN = " + max);
+
+    }
+
+    public static double maxNumber(double x, int y){
+        return x > y? x : y;
+    }
+
+    public static byte maxNumber(byte a,byte b){
+        return a > b ? a : b;
+    }
+
+    public static float maxNumber(float f1, float f2){
+        return f1 > f2 ? f1 : f2;
+    }
+
+    public  static int maxNumber(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static double maxNumber(double a, double b) {
+        return a > b ? a : b;
+    }
+}
