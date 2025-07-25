e23e78b25fd8d015e2ab55a988a900ab3f0af8e1
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-25 14:29:54 +0500

    added new lesson create Array Methods

---
+++ Array Methods/src/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // this is lesson Array methods
+
+
+        int []k = {2,1,3,5,64,0};
+        arrayPrint(k);
+
+        String [] cars = {"AUDI", "MAZDA", "LADA", "CHEVROLET", "TOYOTA"};
+        arrayPrint(cars);
+
+
+
+
+    }
+
+    public static void arrayPrint(int ...n){
+        for(int i : n){
+            System.out.printf("%d, ", i);
+        }
+        System.out.println();
+    }
+
+    public static void arrayPrint(String ...n){
+        for(String i : n){
+            System.out.printf("%s, ", i);
+        }
+        System.out.println();
+    }
+
+
+}
